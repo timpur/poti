f974539a269c8f0649722c9f6e97ee38488bdcba
EstrelApps <carlmatheson779@gmail.com>
2017-04-27 17:36:52 +1000

    Basic HTML Home Page setup

--- poti_assigment_1/poti_assigment_1/index.php
+++ poti_assigment_1/poti_assigment_1/index.php
@@ -1,7 +1,13 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+ini_set('html_errors', 1);
+
+
 $config = include('config.php');
 
+//sqli connection, referrencing objects from config.php
 $conn = new mysqli($config->host, $config->username, $config->password, $config->database);
 
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
@@ -32,6 +38,16 @@ function printFlights($conn, $table){
     <meta charset="utf-8" />
 </head>
 <body>
+        <h1 align="center">Flight Bookings</h1>
+    <footer>
+        <p align="center">DISCLAMER: This website was created for educational purposes as an assignment for the Programming on the Internet subject at the University of Technology Sydney.</p>
+        <table align="center">
+            <button>Search Flights</button>
+        </table>
+        <p align="center">Developed by Students:</p>
+        <p align="center">Tim Purchas</p>
+        <p align="center">Carl Matheson 12551848</p>
+    </footer>
     <?php
         
     ?>
